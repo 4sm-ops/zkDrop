d0cfcd13a5f6eccd0cbb82647fa18bf3fcbbff74
rstm <rstm@gmail.com>
2022-11-16 16:43:33 +0300

    Before migration to Qt

--- zkDrop.py
+++ zkDrop.py
@@ -33,7 +33,7 @@ from lib.keys import load_aleo_keys, restore_keys_from_aleo, pyumbral_encrypt_se
 class MyFrame4 ( wx.Frame ):
 
     def __init__( self, parent ):
-        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 575,750 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 575,790 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
         self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
@@ -44,7 +44,7 @@ class MyFrame4 ( wx.Frame ):
         gbSizer1.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
 
 
-        gbSizer1.Add( ( 0, 25 ), wx.GBPosition( 0, 1 ), wx.GBSpan( 1, 3 ), wx.EXPAND, 5 )
+        gbSizer1.Add( ( 0, 15 ), wx.GBPosition( 0, 1 ), wx.GBSpan( 1, 3 ), wx.EXPAND, 5 )
 
         sbSizer1 = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, wx.EmptyString ), wx.VERTICAL )
 
@@ -97,7 +97,7 @@ class MyFrame4 ( wx.Frame ):
 
         sbSizer5 = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, wx.EmptyString ), wx.VERTICAL )
 
-        self.m_staticText5 = wx.StaticText( sbSizer5.GetStaticBox(), wx.ID_ANY, u"Choose Secret", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText5 = wx.StaticText( sbSizer5.GetStaticBox(), wx.ID_ANY, u"Secret to be used", wx.DefaultPosition, wx.DefaultSize, 0 )
         self.m_staticText5.Wrap( -1 )
 
         sbSizer5.Add( self.m_staticText5, 0, wx.ALL, 5 )
@@ -110,8 +110,23 @@ class MyFrame4 ( wx.Frame ):
 
         gbSizer1.Add( sbSizer5, wx.GBPosition( 5, 1 ), wx.GBSpan( 1, 3 ), wx.EXPAND, 5 )
 
+        sbSizer8 = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, wx.EmptyString ), wx.VERTICAL )
+
+        self.m_staticText81 = wx.StaticText( sbSizer8.GetStaticBox(), wx.ID_ANY, u"Choose file recepient", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText81.Wrap( -1 )
+
+        sbSizer8.Add( self.m_staticText81, 0, wx.ALL, 5 )
+
+        m_choice1Choices = [ u"RustamTwo", u"RustamThree" ]
+        self.m_choice1 = wx.Choice( sbSizer8.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_choice1Choices, 0 )
+        self.m_choice1.SetSelection( 0 )
+        sbSizer8.Add( self.m_choice1, 0, wx.ALL, 5 )
+
+
+        gbSizer1.Add( sbSizer8, wx.GBPosition( 7, 1 ), wx.GBSpan( 1, 3 ), wx.EXPAND, 5 )
+
         self.m_button1 = wx.Button( self, wx.ID_ANY, u"Encrypt and Store", wx.DefaultPosition, wx.DefaultSize, 0 )
-        gbSizer1.Add( self.m_button1, wx.GBPosition( 7, 1 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
+        gbSizer1.Add( self.m_button1, wx.GBPosition( 9, 1 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
 
         sbSizer6 = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, u"Logs" ), wx.VERTICAL )
 
@@ -122,7 +137,7 @@ class MyFrame4 ( wx.Frame ):
         sbSizer6.Add( self.m_staticText8, 0, wx.ALL, 5 )
 
 
-        gbSizer1.Add( sbSizer6, wx.GBPosition( 9, 1 ), wx.GBSpan( 4, 3 ), wx.EXPAND, 5 )
+        gbSizer1.Add( sbSizer6, wx.GBPosition( 10, 1 ), wx.GBSpan( 4, 3 ), wx.EXPAND, 5 )
 
         self.m_bitmap3 = wx.StaticBitmap( self, wx.ID_ANY, wx.Bitmap( u"assets/aleo.png", wx.BITMAP_TYPE_ANY ), wx.DefaultPosition, wx.DefaultSize, 0 )
         self.m_bitmap3.SetMaxSize( wx.Size( 150,50 ) )
@@ -144,8 +159,6 @@ class MyFrame4 ( wx.Frame ):
 
     def __del__( self ):
         pass
-
-
 
 
 
@@ -214,6 +227,9 @@ class MyFrame4 ( wx.Frame ):
         new_file = client.add(self.m_filePicker1.GetPath()+'.encrypted')
 
         self.m_staticText8.SetLabel(self.m_staticText8.GetLabel() + "IPFS data:\n" + str(new_file) + "\n\n")
+
+        self.m_staticText8.Wrap(500)
+
 
         event.Skip()
 
